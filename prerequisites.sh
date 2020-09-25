ca52a22019153f013b51220641d9622a352b566d
virajnemane <65482944+virajnemane@users.noreply.github.com>
2020-09-26 00:01:58 +0530

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo yum install git docker -y
-sudo curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-`uname -s`-`uname -m` -o /usr/bin/docker-compose
-sudo chmod +x /usr/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo systemctl start docker
 sudo systemctl enable docker
